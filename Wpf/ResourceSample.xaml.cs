27588c2d6f5118a1ade48d362394cb2063ee6d39
fmotot <fmotot@gmail.com>
2019-12-12 11:37:23 +0100

    exemple d'accès depuis le code behind

--- Wpf/ResourceSample.xaml.cs
+++ Wpf/ResourceSample.xaml.cs
@@ -23,5 +23,12 @@ namespace Wpf
         {
             InitializeComponent();
         }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            lbResult.Items.Add(pnlMain.FindResource("strPnl").ToString());
+            lbResult.Items.Add(this.FindResource("strWindow").ToString());
+            lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+        }
     }
 }
