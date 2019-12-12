12c7164b78c2f52edf4e3d5dc2a1a275ea98cff3
fmotot <fmotot@gmail.com>
2019-12-12 17:32:37 +0100

    Spellcheck on

---
+++ Wpf/TextBlockWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class TextBlockWindow : Window
+    {
+        public TextBlockWindow()
+        {
+            InitializeComponent();
+
+            TextBlock tb = new TextBlock();
+            tb.TextWrapping = TextWrapping.Wrap;
+            tb.Margin = new Thickness(10);
+            tb.Inlines.Add("An exemple on ");
+            tb.Inlines.Add(new Run("the TextBlock control ") { FontWeight = FontWeights.Bold });
+            tb.Inlines.Add("using ");
+            tb.Inlines.Add(new Run("inline ") { FontStyle = FontStyles.Italic });
+            tb.Inlines.Add(new Run("text formatting ") { Foreground = Brushes.Blue });
+            tb.Inlines.Add("from ");
+            tb.Inlines.Add(new Run("Code-Behind") { TextDecorations = TextDecorations.Underline });
+            tb.Inlines.Add(".");
+
+            grid.Children.Add(tb);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+
+    }
+}
