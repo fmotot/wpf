fdbb63c8143d7c6c1b985ea385a78b2ae50a675b
fmotot <fmotot@gmail.com>
2019-12-12 12:16:29 +0100

    Gestion des exceptions non gérées localement

--- Wpf/App.xaml.cs
+++ Wpf/App.xaml.cs
@@ -13,5 +13,10 @@ namespace Wpf
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("A unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Stop);
+            e.Handled = true;
+        }
     }
 }
