19d59eafb31e1b50dd421287743f37335d94c9fa
fmotot <fmotot@gmail.com>
2019-12-12 11:55:40 +0100

    Exception simple

--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -24,5 +24,20 @@ namespace Wpf
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            try
+            {
+                s.Trim();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            
+        }
     }
 }
